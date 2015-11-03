ea469f00da1b7df806b4de17e4530a98dfa7044d
Dennis Walters <dwalters@engineyard.com>
2015-11-03 09:58:52 -0500

    Initial commit

---
+++ lib/wrapomatic.rb
@@ -0,0 +1,5 @@
+require "wrapomatic/version"
+
+module Wrapomatic
+  # Your code goes here...
+end
---
+++ spec/wrapomatic_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Wrapomatic do
+  it 'has a version number' do
+    expect(Wrapomatic::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
